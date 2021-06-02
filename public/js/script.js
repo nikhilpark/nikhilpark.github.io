6bb786df3fb59cc08f78833adfb366bef31576a5
Nikhil <nikhil.speed@gmail.com>
2021-06-02 14:54:52 +0530

    beta 2

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,19 @@
 const navburg = document.querySelector("#navburg")
 const navOverlay = document.querySelector('#nav-overlay')
-const close  = document.querySelector('#close')
+const close  = document.querySelector('#overlay-nav')
+const s1 = document.querySelector('#s1')
+const s2 = document.querySelector('#s2')
 
 navburg.addEventListener('click',()=>{
-    navOverlay.style.display="block"
+    navOverlay.style.maxHeight='100vh'
+    s1.style.display = 'none'
+    s2.style.display = 'none'
 })
 
 close.addEventListener('click',()=>{
-    navOverlay.style.display="none"
+    navOverlay.style.maxHeight="0"
+    s1.style.display = 'block'
+    s2.style.display = 'block'
 })
 
 
