f5cee64d07bd8a683ee2021d4edda96338b2d026
Nikhil <nikhil.speed@gmail.com>
2021-06-04 22:42:29 +0530

    kinda responsive

--- public/js/script.js
+++ public/js/script.js
@@ -4,8 +4,9 @@ const close  = document.querySelector('#overlay-nav')
 const s1 = document.querySelector('#s1')
 const s2 = document.querySelector('#s2')
 
+
 navburg.addEventListener('click',()=>{
-    navOverlay.style.maxHeight='100vh'
+    navOverlay.style.maxHeight='200vh'
     s1.style.display = 'none'
     s2.style.display = 'none'
 })
@@ -16,7 +17,13 @@ close.addEventListener('click',()=>{
     s2.style.display = 'block'
 })
 
-
+document.querySelectorAll('.nlink').forEach(item => {
+    item.addEventListener('click',()=>{
+        navOverlay.style.maxHeight="0"
+        s1.style.display = 'block'
+        s2.style.display = 'block'
+    })
+})
 
 // $('#navburg').click(()=>{
 //     $('#nav-overlay').attr('style','display:block')
