516806a2d590859b6dc52611c150abfa64ebc532
Nikhil Kumar <1703322.it.cec@cgc.edu.in>
2021-07-23 00:26:06 +0530

    added skills section

--- public/js/script.js
+++ public/js/script.js
@@ -3,26 +3,23 @@ const navOverlay = document.querySelector("#nav-overlay");
 const close = document.querySelector("#overlay-nav");
 const projects = document.querySelector("#projects-cont");
 const s1 = document.querySelector("#s1");
-const s2 = document.querySelector("#s2");
+const s3 = document.querySelector("#s3");
 const red = document.querySelector("#red");
 
 navburg.addEventListener("click", () => {
   navOverlay.style.maxHeight = "200vh";
-  s1.style.display = "none";
-  s2.style.display = "none";
+
 });
 
 close.addEventListener("click", () => {
   navOverlay.style.maxHeight = "0";
-  s1.style.display = "block";
-  s2.style.display = "block";
+
 });
 
 document.querySelectorAll(".nlink").forEach((item) => {
   item.addEventListener("click", () => {
     navOverlay.style.maxHeight = "0";
-    s1.style.display = "block";
-    s2.style.display = "block";
+
   });
 });
 
@@ -31,7 +28,7 @@ let currentPos = window.pageYOffset;
 const update = () => {
   const newPos = window.pageYOffset;
   const diff = newPos - currentPos;
-  const speed = diff * 0.66;
+  const speed = diff * 0.2;
 
   projects.style.transform = `skewY(${speed}deg)`;
 
