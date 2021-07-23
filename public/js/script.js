78dcf4bc9a15dd2b29bf9c10250c3baf9c001e25
Nikhil Kumar <1703322.it.cec@cgc.edu.in>
2021-07-23 14:31:21 +0530

    fixed navbar

--- public/js/script.js
+++ public/js/script.js
@@ -2,10 +2,13 @@ const navburg = document.querySelector("#navburg");
 const navOverlay = document.querySelector("#nav-overlay");
 const close = document.querySelector("#overlay-nav");
 const projects = document.querySelector("#projects-cont");
+
 const s1 = document.querySelector("#s1");
+const s2 = document.querySelector("#s2");
 const s3 = document.querySelector("#s3");
 const red = document.querySelector("#red");
-
+console.log(skillsarrow);
+  
 navburg.addEventListener("click", () => {
   navOverlay.style.maxHeight = "200vh";
 
@@ -15,6 +18,8 @@ close.addEventListener("click", () => {
   navOverlay.style.maxHeight = "0";
 
 });
+
+
 
 document.querySelectorAll(".nlink").forEach((item) => {
   item.addEventListener("click", () => {
